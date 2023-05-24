4bb9f181d9432bc3f5fc887f64994f21cfd08923
nibon7 <nibon7@163.com>
2023-05-24 14:33:19 +0800

    test: move helper functions to common.rs

--- tests/common.rs
+++ tests/common.rs
@@ -1,4 +1,23 @@
+#![allow(unused)]
+
 use clap::{builder::PossibleValue, Arg, ArgAction, Command, ValueHint};
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::sync::Arc;
+
+use nu_cli::NuCompleter;
+use nu_command::create_default_context;
+use nu_parser::parse;
+use nu_protocol::{
+    engine::{EngineState, Stack, StateWorkingSet},
+    Value,
+};
+use nu_test_support::fs;
+
+use reedline::Suggestion;
+
+const SEP: char = std::path::MAIN_SEPARATOR;
 
 pub fn basic_command(name: &'static str) -> Command {
     Command::new(name)
@@ -254,4 +273,108 @@ pub fn assert_matches_path(
         .action_env("SNAPSHOTS")
         .normalize_paths(false)
         .matches_path(expected_path, buf);
+}
+
+// creates a new engine with the current path into the completions fixtures folder
+fn new_engine() -> (PathBuf, EngineState, Stack) {
+    // Target folder inside assets
+    let mut dir = fs::root().join("tests");
+    dir.push("snapshots");
+
+    let mut dir_str = dir
+        .clone()
+        .into_os_string()
+        .into_string()
+        .unwrap_or_default();
+    dir_str.push(SEP);
+
+    // Create a new engine with default context
+    let mut engine_state = create_default_context();
+
+    // New stack
+    let mut stack = Stack::new();
+
+    // Add pwd as env var
+    stack.add_env_var(
+        "PWD".to_string(),
+        Value::String {
+            val: dir_str.clone(),
+            span: nu_protocol::Span::new(0, dir_str.len()),
+        },
+    );
+
+    #[cfg(windows)]
+    stack.add_env_var(
+        "Path".to_string(),
+        Value::String {
+            val: "c:\\some\\path;c:\\some\\other\\path".to_string(),
+            span: nu_protocol::Span::new(0, dir_str.len()),
+        },
+    );
+
+    #[cfg(not(windows))]
+    stack.add_env_var(
+        "PATH".to_string(),
+        Value::String {
+            val: "/some/path:/some/other/path".to_string(),
+            span: nu_protocol::Span::new(0, dir_str.len()),
+        },
+    );
+
+    // Merge environment into the permanent state
+    let merge_result = engine_state.merge_env(&mut stack, &dir);
+    assert!(merge_result.is_ok());
+
+    (dir, engine_state, stack)
+}
+
+// match a list of suggestions with the expected values
+pub fn match_suggestions(expected: Vec<String>, suggestions: Vec<Suggestion>) {
+    let expected_len = expected.len();
+    let suggestions_len = suggestions.len();
+    if expected_len != suggestions_len {
+        panic!(
+            "\nexpected {expected_len} suggestions but got {suggestions_len}: \n\
+            Suggestions: {suggestions:#?} \n\
+            Expected: {expected:#?}\n"
+        )
+    }
+    expected.iter().zip(suggestions).for_each(|it| {
+        assert_eq!(it.0, &it.1.value);
+    });
+}
+
+pub fn external_completion(file_name: &str) -> NuCompleter {
+    // Create a new engine
+    let (dir, mut engine_state, mut stack) = new_engine();
+
+    let path = dir.join(file_name);
+    let mut buf = Vec::new();
+    let mut file =
+        File::open(&path).unwrap_or_else(|_| panic!("Failed to open {}", path.display()));
+    file.read_to_end(&mut buf)
+        .unwrap_or_else(|_| panic!("Failed to open {}", path.display()));
+
+    let (_, delta) = {
+        let mut working_set = StateWorkingSet::new(&engine_state);
+        let block = parse(&mut working_set, None, &buf, false);
+        assert!(working_set.parse_errors.is_empty());
+
+        (block, working_set.render())
+    };
+
+    assert!(engine_state.merge_delta(delta).is_ok());
+
+    // Merge environment into the permanent state
+    assert!(engine_state.merge_env(&mut stack, &dir).is_ok());
+
+    let latest_block_id = engine_state.num_blocks() - 1;
+
+    // Change config adding the external completer
+    let mut config = engine_state.get_config().clone();
+    config.external_completer = Some(latest_block_id);
+    engine_state.set_config(&config);
+
+    // Instantiate a new completer
+    NuCompleter::new(Arc::new(engine_state), stack)
 }
